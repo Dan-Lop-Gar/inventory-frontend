dd021671396997f668301d1ea46ca6cfd41d1777
Daniel Lopez <daniel.lopez.garduno@gmail.com>
2026-05-11 17:02:21 -0600

    Updated backend service port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiRestUrl: 'http://backend:8080/api',
+  apiRestUrl: 'http://backend:8081/api',
   apiGrpcUrl: 'http://envoy:8090/api/grpc',
   keycloak: {
     url: 'http://keycloak:8080',
