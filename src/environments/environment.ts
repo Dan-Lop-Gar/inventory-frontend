8773f4c2b4c8d399bd39faf74b2e4caebba8661a
Daniel Lopez <daniel.lopez.garduno@gmail.com>
2026-07-02 01:06:14 -0600

    Resolved conflict by chosing correct url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiRestUrl: 'http://localhost/api',
+  apiRestUrl: 'http://backend:8081/api',
   apiGrpcUrl: 'http://envoy/api/grpc',
   keycloak: {
     url: 'http://keycloak:8180',
