abb0cb5cfde79149d035ad6f50fd70e7a6df6795
Daniel Lopez <daniel.lopez.garduno@gmail.com>
2026-07-02 01:04:31 -0600

    Resolved conflict

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // apiRestUrl: 'http://inventory-backend/api',
   apiRestUrl: 'http://localhost/api',
   apiGrpcUrl: 'http://envoy/api/grpc',
   keycloak: {
